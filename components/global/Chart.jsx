76b139b6e2f4f6292e30da297a94a94387186023
Tausif <tausif@Tausifs-Mac-mini.local>
2025-09-15 02:13:48 +0600

    balance updated

--- components/global/Chart.jsx
+++ components/global/Chart.jsx
@@ -16,22 +16,24 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from '@/components/ui/chart'
+import { useEffect, useState } from 'react'
+import { getChartData } from '@/actions/expense.action'
 
 export const description = 'A multiple bar chart'
 
 const chartData = [
-  { month: 'January', desktop: 186, mobile: 80 },
-  { month: 'February', desktop: 305, mobile: 200 },
-  { month: 'March', desktop: 237, mobile: 120 },
-  { month: 'April', desktop: 73, mobile: 190 },
-  { month: 'May', desktop: 209, mobile: 130 },
-  { month: 'June', desktop: 214, mobile: 140 },
-  { month: 'July', desktop: 214, mobile: 140 },
-  { month: 'August', desktop: 12, mobile: 3 },
-  { month: 'September', desktop: 34, mobile: 45 },
-  { month: 'October', desktop: 56, mobile: 1 },
-  { month: 'November', desktop: 12, mobile: 23 },
-  { month: 'December', desktop: 23, mobile: 12 },
+  { month: 'January', withdraw: 40, deposite: 80 },
+  { month: 'February', withdraw: 35, deposite: 65 },
+  { month: 'March', withdraw: 111, deposite: 120 },
+  { month: 'April', withdraw: 43, deposite: 90 },
+  { month: 'May', withdraw: 32, deposite: 12 },
+  { month: 'June', withdraw: 33, deposite: 55 },
+  { month: 'July', withdraw: 80, deposite: 30 },
+  { month: 'August', withdraw: 12, deposite: 3 },
+  { month: 'September', withdraw: 34, deposite: 45 },
+  { month: 'October', withdraw: 56, deposite: 1 },
+  { month: 'November', withdraw: 12, deposite: 23 },
+  { month: 'December', withdraw: 23, deposite: 12 },
 ]
 
 const chartConfig = {
@@ -45,7 +47,7 @@ const chartConfig = {
   },
 }
 
-export function ChartDashboard() {
+export const ChartDashboard = () => {
   return (
     <Card className='shadow-none border-none p-0'>
       <CardHeader className='p-0'>
@@ -69,8 +71,8 @@ export function ChartDashboard() {
               cursor={false}
               content={<ChartTooltipContent indicator='dashed' />}
             />
-            <Bar dataKey='desktop' fill='var(--color-desktop)' radius={4} />
-            <Bar dataKey='mobile' fill='var(--color-mobile)' radius={4} />
+            <Bar dataKey='withdraw' fill='var(--color-desktop)' radius={4} />
+            <Bar dataKey='deposite' fill='var(--color-mobile)' radius={4} />
           </BarChart>
         </ChartContainer>
       </CardContent>
--- models/expense-model.js
+++ models/expense-model.js
@@ -7,7 +7,7 @@ const expenseSchema = new mongoose.Schema(
       required: true,
     },
     amount: {
-      type: String,
+      type: Number,
       required: true,
     },
   },
